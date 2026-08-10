42a0d05ed86331c2a078fbd23f841f7e58d7fd42
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-08-10 16:31:06 +0300

    feat: add responsive Hero Search Section

--- dwello-real-estate/src/Components/HeroSearchSection.jsx
+++ dwello-real-estate/src/Components/HeroSearchSection.jsx
@@ -4,63 +4,38 @@ import home from '../assets/icons/home.png'
 import price from '../assets/icons/price.png'
 
 function HeroSearchSection() {
-return (
-    <>
-    <div className='bg-[#FEF7F2] h-17.5'>
-        <div className="flex bg-[#DDC7BB] w-237.5 mb-34 ml-35 rounded-2xl py-10 pl-7 pr-10 focus:outline-none shadow-sm">
-            <div className="relative w-55">
-                <input
-                type="text"
-                placeholder="Location"
-                className="w-full bg-[#FAF5F0] text-[#695346] placeholder-[#695346] font-bold py-3 pl-4 pr-10 rounded-lg focus:outline-none shadow-sm "
-            />
-                <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                <img 
-                    src={location} 
-                    alt="Location" 
-                    className="w-[14.6px] h-[17.5px] object-contain"
-                />
+    return (
+        <div className='bg-[#FBF5F1] sm:mb-40 h-44 lg:h-15'> 
+            <div className='bg-[#DDC7BB] w-86 h-88 m-auto rounded-2xl lg:flex lg:w-230 lg:h-30 py-10 lg:m-auto items-center'>
+                <div className="relative w-48 h-12 bg-[#FBF5F1] rounded-lg flex items-center -mt-1.25 ml-20 my-8 lg:m-auto">
+                    <input type="text" placeholder="Location" className="w-full h-full bg-transparent pl-4 pr-10 text-sm font-bold outline-none placeholder:text-[#695346]"/>
+                    <button className="absolute right-3">
+                    <img src={location} alt="Location" className="w-5 h-5" />
+                    </button>
+                </div>
+
+                <div className="relative w-48 h-12 bg-[#FBF5F1] rounded-lg flex items-center -mt-1.25 ml-20 my-8 lg:m-auto">
+                    <input type="text" placeholder="Type" className="w-full h-full bg-transparent pl-4 pr-10 text-sm font-bold outline-none placeholder:text-[#695346]"/>
+                    <button className="absolute right-3">
+                    <img src={home} alt="Location" className="w-5 h-5" />
+                    </button>
+                </div>
+
+                <div className="relative w-48 h-12 bg-[#FBF5F1] rounded-lg flex items-center -mt-1.25 ml-20 my-8 lg:m-auto">
+                    <input type="text" placeholder="Price Range" className="w-full h-full bg-transparent pl-4 pr-10 text-sm font-bold outline-none placeholder:text-[#695346]"/>
+                    <button className="absolute right-3">
+                    <img src={price} alt="Location" className="w-5 h-5" />
+                    </button>
+                </div>
+                
+                <div className='w-39 h-14 rounded-lg ml-30 flex justify-between items-center mt-8 lg:m-10'>
+                    <button className='w-30.5 h-12 bg-[#3B2418] text-white font-semibold rounded-lg px-8 py-2 hover:opacity-90 transition-all duration-300 mt-2 shadow-sm'>
+                        Sign up
+                    </button>
                 </div>
             </div>
-
-            <div className="relative w-55 ml-5.75">
-                <input
-                type="text"
-                placeholder="Type"
-                className="w-full bg-[#FAF5F0] text-[#695346] placeholder-[#695346] font-bold py-3 pl-4 pr-10 rounded-lg focus:outline-none shadow-sm "
-            />
-                <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                <img 
-                    src={home} 
-                    alt="Location" 
-                    className="w-4 h-[15.81px] object-contain"
-                />
-                </div>
-            </div>
-
-            <div className="relative w-55 ml-5.75">
-                <input
-                type="text"
-                placeholder="Price Range"
-                className="w-full bg-[#FAF5F0] text-[#695346] placeholder-[#695346] font-bold py-3 pl-4 pr-10 rounded-lg focus:outline-none shadow-sm "
-            />
-                <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                <img 
-                    src={price} 
-                    alt="Location" 
-                    className="w-5.5 h-5.5 object-contain"
-                />
-                </div>
-            </div>
-
-            <a href="#">
-            <button className="w-39 h-12 ml-15 bg-[#3B2418] text-white font-semibold rounded-lg hover:opacity-90 transition cursor-pointer">Sign up
-            </button>
-            </a>
         </div>
-    </div>
-    </>
-)
+    )
 }
 
 export default HeroSearchSection
